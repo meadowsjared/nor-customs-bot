5279c9b0a1e8126908a2e4d22fa66fd4cfb91ce7
Jared Meadows <meadowsjared@gmail.com>
2025-05-28 19:20:50 -0700

    refactor: update announce function to accept flags and improve type definitions
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- utils/announce.ts
+++ utils/announce.ts
@@ -1,16 +1,24 @@
-import { Interaction } from 'discord.js';
+import { ButtonInteraction, CacheType, ChatInputCommandInteraction, MessageCreateOptions } from 'discord.js';
 import { botChannelName } from '../constants';
+
+type InteractionReplyOptionsFlags = MessageCreateOptions['flags'];
 
 /**
  * Announces a message in the bot channel.
  * @param interaction The interaction object from Discord, either a ChatInputCommandInteraction or ButtonInteraction.
  * @param message The message to announce in the bot channel.
+ * @param flags Optional flags for the message, such as `EPHEMERAL` to make it visible only to the user.
  */
-export async function announce(interaction: Interaction, message: string) {
+export async function announce(
+  interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction<CacheType>,
+  message: string,
+  flags?: InteractionReplyOptionsFlags
+) {
   const channel = interaction.guild?.channels.cache.find(ch => ch.name === botChannelName);
   if (channel && channel.isTextBased()) {
     await channel.send({
       content: message,
+      flags,
     });
   }
 }
