0efacea600d1d87be3165ef9ebbbd468d0e01404
Jared Meadows <meadowsjared@gmail.com>
2025-08-03 21:39:13 -0700

    fix: update lobby status message format to include player roles and display names
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- commands/index.ts
+++ commands/index.ts
@@ -55,11 +55,15 @@ function generateLobbyStatusMessage(pPreviousPlayersList?: string): string {
   const activePlayers = getActivePlayers();
   const lobbyPlayers = activePlayers
     .filter(p => p?.team === undefined)
-    .map((p, index) => `${index + 1}: <@${p.discordId}>`);
+    .map(
+      (p, index) =>
+        `${index + 1}: @${p.usernames.discordDisplayName}: (${p.usernames.hots}) \`${getPlayerRolesFormatted(p.role)}\``
+    );
   const lobbyMessage = lobbyPlayers.join('\n');
 
   // combine the lobbyPlayers and previousPlayersList, into one string, labeling each section, but skip a section if there are no players in that section
   const playerListWithLabels = [];
+  playerListWithLabels.push(`A new game has started! All players have been marked as inactive.`);
   if (previousPlayersList) playerListWithLabels.push(`**Previous Players**:\n${previousPlayersList}`);
   playerListWithLabels.push(
     `**Players in the lobby**: (${lobbyPlayers.length})\n${lobbyMessage || 'The lobby is empty.'}`
@@ -69,7 +73,7 @@ function generateLobbyStatusMessage(pPreviousPlayersList?: string): string {
   }
   const playerListWithLabelsString = playerListWithLabels.join('\n');
 
-  return `${playerListWithLabelsString}\nA new game has started! All players have been marked as inactive.\nPlease click below if you are going to play.`;
+  return `${playerListWithLabelsString}\n\nPlease click below if you are going to play.`;
 }
 
 /** generates the list of previous players */
